a6c7c7096ce9e4163781473a28cc8164276111e0
suraviregmi <regmisuravi@gmail.com>
2017-05-23 23:07:22 +0545

    templates

---
+++ data.py
@@ -0,0 +1,9 @@
+
+#from flask import Flask, render_template
+#import os, json, csv
+
+#def showjson():
+ #   SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+  #  json_url = os.path.join(SITE_ROOT, "static/data", "data.json")
+   # details = json.load(open(json_url))
+    #return render_template('showjson.jade', details=details)
