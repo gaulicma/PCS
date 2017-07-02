59f075fb6a80d72a0c9b14838827ed412a0ed71d
shradhaN <shradha.neupane@yahoo.com>
2017-07-02 13:06:43 +0545

    added database to muncha, and some changes to satodeal xpaths which is not complete,

--- munchaScraper.py
+++ munchaScraper.py
@@ -5,11 +5,26 @@ import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
+import sqlite3
+conn = sqlite3.connect('test.db')
+cur = conn.cursor()
+
+#created the table once and then commented it out
+
+
+#conn.execute('''CREATE TABLE PRODUCT_DETAILS
+#			(
+#			LINK CHAR(255) NOT NULL,
+#			NAME CHAR(255),
+#			PRICE REAL,
+#			IMG_SRC CHAR(255));''')
+
+
 
 
 def MunchaScraper(product_keyword):
 	#product_keyword = input ('what do you want to search')
-
+	#conn.open()
 	print('MUNCHA \n\n\n')
 	#grab the url
 	my_url = 'http://www.shop.muncha.com/Search.aspx?MID=1&q=' + product_keyword
@@ -26,25 +41,32 @@ def MunchaScraper(product_keyword):
 	containers = page_soup.findAll("div",{"class":"panel panel-default"})
 	 
 
-	filename = 'muncha.csv'
+	'''filename = 'muncha.csv'
 	f = open(filename, 'w')
 	headers =  'link, product_name, prooduct_price \n'
-	#f.write(headers)
+	#f.write(headers)'''
 
 
 	#grabs individual product
 	for contain in containers:
+		
 		 link = contain.a['href']
+		 img_src = contain.img['src']
 
 		 name = contain.div.a.img['alt']
 		 
 		 price_container = contain.findAll("div",{"class":"price-desc"})
 		 price = price_container[0].text.strip()
 
-
-		 print('link '+ link)
-		 print('name '+ name)
-		 print('price ' + price)
-	 
-		 f.write(link +',' + name.replace(',','| ') +',' +  price.replace(',',' ') + '\n')
-	f.close()
+		 cur.execute("""INSERT INTO PRODUCT_DETAILS(LINK, NAME, PRICE , IMG_SRC)\
+		 	VALUES(?,?,?,? )""", [link, name, price, img_src]);
+		 print('link '+ link + '\n')
+		 print('image ' + img_src+ '\n')
+		 print('name '+ name+ '\n')
+		 print('price ' + price+ '\n')
+		 conn.commit()
+		 print ("records added")
+		 #conn.close() 
+		 #f.write(link +',' + name.replace(',','| ') +',' +  price.replace(',',' ') + '\n')
+	conn.close()
+MunchaScraper("earphones")
